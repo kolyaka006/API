7b7b87302c8a903bc3b802349934b126d4fbd102
kolyaka006 <jigasan70@gmail.com>
2016-03-09 10:07:09 +0200

    Update index.js

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ server.post('/endpoint/post_response', function (req, res, next) {
 });
 
 server.get('/endpoint/response_codes', function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin','*');
     var numb =  Math.floor(Math.random() * (100)%2);
     if(numb) {
         res.send(200,{result:true,text:"Success!"})
@@ -24,6 +25,7 @@ server.get('/endpoint/response_codes', function (req, res, next) {
 });
 
 server.get('/endpoint/data_set', function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin','*');
     var numb =  Math.floor(Math.random() * (100)%2);
     if(numb) {
         var plant =  Math.floor(Math.random() * (100)%3);
@@ -31,6 +33,7 @@ server.get('/endpoint/data_set', function (req, res, next) {
             case 0: res.send(200,{item: "apple", type: "fruit"})
             case 1: res.send(200,{item: "pear", type: "fruit"})
             case 2: res.send(200,{item: "banana", type: "fruit"})
+            default: break;
         }
     }
     else {
@@ -38,6 +41,7 @@ server.get('/endpoint/data_set', function (req, res, next) {
         switch (plant) {
             case 0: res.send(200,{item: "cucumber", type: "vegetable"})
             case 1: res.send(200,{item: "potatoes", type: "vegetable"})
+            default: break;
         }
     }
     return next();
